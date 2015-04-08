f50b82ec74d8ca59054db55ea2b19eca01dc0187
Matthew McKenna <mpmckenna8@me.com>
2015-04-07 20:53:12 -0700

    tring to make a github pages branch to show off my page to the world.

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # catcount
-Cat Counter mad for https://www.udacity.com/course/viewer#!/c-ud989/l-3417188540/m-3438178949
+
+Cat Counter made for https://www.udacity.com/course/viewer#!/c-ud989/l-3417188540/m-3438178949
+
+
+Each commit should be something like a different lesson from the course.  So if you want to see how it
+looked at previous steps just look at the commit history.
+
+After they started wanting a list which changed the page when clicked I decided to
+use some d3 to stop me from making a bunch of code.  Though I kept some of it kind of spread out
+to show what I was doing.
+
+
+The cat images come from those provided in the instructions for this course and me.
+Plus the ascii cat is from a repo with a ton of cat images in case you ever need some.
+I think they're all even geotaged if you wanna map em.
+
+https://github.com/maxogden/cats
--- catpic.js
+++ catpic.js
@@ -1,38 +1,98 @@
 var blah = 'blah';
 
+
+
 cats = {
-  'chewie':{
-    "finame":"chewie.jpg",
+  "Xuxa":{
+    "finame": "cat.jpg",
+    "id": "xu",
+    "count": 0,
+    "count_id": "cout",
+    "name":"xuxu"
+  }
+  ,'chewie':{
+    "finame":"chew.jpg",
     "id":"chew",
     "count": 0,
-    "count_id":"chi"
-  }
+    "count_id":"chi",
+    "name": "chewie"
+  },
+
+
 }
 
+console.log(cats)
 
-var kittie = document.getElementsByClassName('kittie')
+var ascii = ( new Cat('jgscat.jpg',  'ascii'));
+var onname = ascii.name;
 
-var count = 0;
+cats.ascii = ascii;
+var kitties = new Cat('kitties.jpg', 'kitties');
 
-kittie[0].addEventListener('click', function(){
-  count++;
-  console.log(count);
-  document.getElementById('cout').textContent = count;
+cats.kitties = kitties;
 
-}, false)
+cats.artcats = new Cat('artcats.jpg', 'Artsy')
+
+console.log(Object.keys(cats));
+
+
+
+var arcat = Object.keys(cats);
+
 
 
 var chew = document.getElementById('chew');
 
-incOnCli(chew,cats.chewie);
+//incOnCli(chew,cats.chewie);
 
-function incOnCli(ele, iden){
 
-  ele.addEventListener('click', function(){
-    iden.count++
-    var toch = document.getElementById(iden.count_id)
-    toch.textContent = iden.count;
+d3.select('.catli').selectAll("li")
+.data(arcat)
+.enter()
+.append('li')
+.html(function(d){
+  console.log(d)
 
-  })
+  return d
+})
+.on('click', function(d){
+  console.log('thing was clicked', cats[d])
+
+  //this.attr('background', 'blue')
+console.log(this)
+  d3.selectAll('li')
+    .style('background', null)
+
+  d3.select(this)
+    .style('background', 'yellow')
+
+  d3.select('.catName')
+  .text(cats[d].name)
+
+  d3.select('#cout')
+   .text(cats[d].count)
+
+   d3.select('.kittie')
+    .attr('src', 'pics/' + cats[d].finame)
+    .on('click', function(){
+
+      cats[d].count++
+      console.log(cats[d].count)
+
+      d3.select('#cout')
+        .text(cats[d].count);
+    })
+})
+
+
+
+
+
+
+function Cat(filename, name){
+
+  this.finame = filename;
+  this.count = 0;
+  this.name = name;
 
 }
