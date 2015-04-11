c36f49d6e38adc72881e03bc0592f0e92a1263e1
Matthew McKenna <mpmckenna8@me.com>
2015-04-10 20:10:35 -0700

    Making it not need libraries and it is working but need to do th final admin button still

--- catpic.js
+++ catpic.js
@@ -3,90 +3,140 @@ var blah = 'blah';
 
 
 cats = {
-  "Xuxa":{
-    "finame": "cat.jpg",
-    "id": "xu",
-    "count": 0,
-    "count_id": "cout",
-    "name":"xuxu"
-  }
-  ,'chewie':{
-    "finame":"chew.jpg",
-    "id":"chew",
-    "count": 0,
-    "count_id":"chi",
-    "name": "chewie"
+  liscat: [],
+  addcat:function(filename, name){
+
+    cats.liscat.push(new Cat(filename,name));
+
   },
+  currcat:null
 
 
 }
 
+cats.addcat('cat.jpg', 'xuxu');
+cats.addcat('chew.jpg', 'chewie');
+
+
+
+
+
+var currentcat = null;
+
 console.log(cats)
 
-var ascii = ( new Cat('jgscat.jpg',  'ascii'));
-var onname = ascii.name;
+cats.addcat('jgscat.jpg',  'ascii');
 
-cats.ascii = ascii;
-var kitties = new Cat('kitties.jpg', 'kitties');
+cats.addcat('kitties.jpg', 'kitties');
 
-cats.kitties = kitties;
 
-cats.artcats = new Cat('artcats.jpg', 'Artsy')
+cats.addcat('artcats.jpg', 'Artsy')
 
 console.log(Object.keys(cats));
 
 
+// this is kind of my list model
 
-var arcat = Object.keys(cats);
 
 
+// octopus
+
+var octopus = {
+  init: function(){
+    // make the current cat the first cat in the list
+    cats.currcat = cats.liscat[0];
+
+  //  console.log(cats)
+    catListV.init();
+    catview.init()
+  },
+  getCats:function(){
+    return cats.liscat;
+  },
+  currentCat: function(){
+    return cats.currcat;
+  },
+  setcurrcat: function(cat){
+    cats.currcat = cat;
+  },
+  incrementCou:function(){
+    cats.currcat.count++;
+    catview.render();
+  }
+
+
+}
+
+var catListV = {
+  init:function(){
+
+    this.catListElem = document.getElementsByClassName('catli')[0];
+
+    this.render()
+  },
+
+  render: function(){
+    var cat, i, elem;
+
+    var kitties = octopus.getCats();
+
+    console.log(kitties);
+
+    for(i = 0; i < kitties.length; i++){
+      cat = kitties[i];
+      console.log(cat)
+      elem = document.createElement('li');
+
+      elem.textContent = cat.name;
+
+      elem.addEventListener('click', (function(catcopy){
+        return function(){
+          octopus.setcurrcat(catcopy);
+          catview.render();
+        };
+      })(cat))
+
+      this.catListElem.appendChild(elem);
+    }
+
+  }
+}
+
+var catview = {
+  init:function(){
+    // point to stuff
+    this.catElem = document.getElementsByClassName('catpic')[0];
+    this.catNameElem = document.getElementsByClassName('catName')[0]
+    this.catImageElem = document.getElementById('xu')
+    this.countElem = document.getElementById('cout')
+
+    console.log(this.catElem)
+
+    this.catImageElem.addEventListener('click',function(){
+      octopus.incrementCou();
+    })
+
+    this.render();
+
+  },
+
+  render: function(){
+    console.log('now to make them cats show')
+    var hey = octopus.currentCat();
+    console.log(hey);
+
+    this.countElem.textContent = hey.count;
+    this.catImageElem.src = 'pics/' + hey.finame;
+    this.catNameElem.textContent = hey.name;
+
+  }
+}
 
 var chew = document.getElementById('chew');
 
 //incOnCli(chew,cats.chewie);
 
-
-d3.select('.catli').selectAll("li")
-.data(arcat)
-.enter()
-.append('li')
-.html(function(d){
-  console.log(d)
-
-  return d
-})
-.on('click', function(d){
-  console.log('thing was clicked', cats[d])
-
-  //this.attr('background', 'blue')
-console.log(this)
-  d3.selectAll('li')
-    .style('background', null)
-
-  d3.select(this)
-    .style('background', 'yellow')
-
-  d3.select('.catName')
-  .text(cats[d].name)
-
-  d3.select('#cout')
-   .text(cats[d].count)
-
-   d3.select('.kittie')
-    .attr('src', 'pics/' + cats[d].finame)
-    .on('click', function(){
-
-      cats[d].count++
-      console.log(cats[d].count)
-
-      d3.select('#cout')
-        .text(cats[d].count);
-    })
-})
-
-
-
-
+octopus.init();
 
 
 function Cat(filename, name){
